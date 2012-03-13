ac0445f7bdf3f8f1dfdc22fbf33ba0d5c7abf420
locojaydev <locojaydev@dev.com>
2012-03-12 21:50:09 -0400

    first commit

---
+++ README.markdown
@@ -0,0 +1,12 @@
+MailDir Growl
+=============
+
+posts a [Growl](http://growl.info) notification whenever a new Mail is detected....
+
+usage:
+
+    maildirgrowl.py -m ~/Mail/gmail/INBOX/new
+
+if you use offlineimap just add this to your cron or launchct
+
+
---
+++ maildirgrowl.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import email
+import subprocess
+from optparse import OptionParser
+#from dateutil.parser import parse as dateparser
+import os
+
+imagefile = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+        '77.png')
+
+
+def growl_message(message):
+
+    msg_from = message['From']
+    name, address = email.utils.parseaddr(msg_from)
+    msg_subject = message['Subject']
+    # msg_date = dateparser(message['Date'])
+
+    message = '%s\n%s' % (name, msg_subject)
+    subprocess.check_call(['growlnotify',
+                     '-m', message,
+                     '--image', imagefile])
+
+
+def main():
+
+    usage = 'usage: %prog [options] user -h for help'
+    parser = OptionParser(usage)
+    parser.add_option('-m' , '--maildir', action='store',
+            type='string', dest='maildir', help='path to dir to check mails')
+    (options, args) = parser.parse_args()
+
+    if options.maildir:
+        for file_ in os.listdir(options.maildir):
+            fullpath = os.path.join(options.maildir, file_)
+            with open(fullpath) as mail_file:
+                msg = email.message_from_file(mail_file)
+                growl_message(msg)
+
+if __name__ == '__main__':
+    import sys
+    status = main()
+    sys.exit(status)
