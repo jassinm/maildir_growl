fde7b39032a1380c2664ba6eec2c897c7d42fba9
Chris Moultrie <tebriel@gmail.com>
2012-10-08 22:30:33 -0400

    Takes in a command line argument and handles None

--- maildirgrowl.py
+++ maildirgrowl.py
@@ -29,6 +29,8 @@ def growl_message(message):
 
 def is_too_old(file_path, max_minutes):
 
+    if max_minutes == 0:
+        return False
     file_time = os.stat(file_path).st_mtime
     current_time = time.time()
     if ((current_time - file_time) / 60) > max_minutes:
@@ -47,10 +49,15 @@ def main():
             type='float', dest='max_minutes', help='greater than this value prevents growl message')
     (options, args) = parser.parse_args()
 
+    if options.max_minutes:
+        max_age = options.max_minutes
+    else:
+        max_age = 0
+
     if options.maildir:
         for file_ in os.listdir(options.maildir):
             fullpath = os.path.join(options.maildir, file_)
-            if is_too_old(fullpath, options.max_minutes):
+            if is_too_old(fullpath, max_age):
                 continue
             with open(fullpath) as mail_file:
                 msg = email.message_from_file(mail_file)
